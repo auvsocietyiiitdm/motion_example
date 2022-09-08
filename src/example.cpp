fd60ef1cb6c8d0167df29ce53c8ca7a45503da78
Rahulkannan S <ec20b1063@iiitdm.ac.in>
2022-09-09 00:01:08 +0530

    submodule change

--- src/example.cpp
+++ src/example.cpp
@@ -6,12 +6,24 @@ ThrusteredVehicleMotionController cholan;
 
 int main(){
     cholan.setSurgeControlMode(OPEN_LOOP_MODE);
+    cholan.setSurgeThrust(0);
+        cholan.updateThrustValues();
+        for (int  i = 0; i < 16; i++)
+        {
+            cholan.refresh();
+                std::this_thread::sleep_for(std::chrono::milliseconds(7));
+
+        }
+
+    std::this_thread::sleep_for(std::chrono::seconds(7));
+     
+
 
     while (true)
     {
         for (int  i = 0; i < 16; i++)
         {
-             cholan.setSurgeThrust(20 + 5*i);
+             cholan.setSurgeThrust(100);
             cholan.updateThrustValues();
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
